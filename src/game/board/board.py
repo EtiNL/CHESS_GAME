e0bf1ef33406d7dc2878b1afb835b674ee192f4f
Etienne Lanzeray <etienne.lanzeray@gmail.com>
2023-05-18 17:24:42 +0200

    transfert code from the notebook to the game package

---
+++ src/game/board/board.py
@@ -0,0 +1,223 @@
+from ..pieces.pawn import Pawn
+from ..pieces.king import King
+from ..pieces.queen import Queen
+from ..pieces.bishop import Bishop
+from ..pieces.knight import Knight
+from ..pieces.rook import Rook
+
+
+
+class Board:
+    def __init__(self,newgame,position):
+        if newgame:
+
+            #create the white pieces
+            #wAp = white A pawn ...
+            wAp,wBp,wCp,wDp,wEp,wFp,wGp,wHp=Pawn('w',1,2),Pawn('w',2,2),Pawn('w',3,2),Pawn('w',4,2),Pawn('w',5,2),Pawn('w',6,2),Pawn('w',7,2),Pawn('w',8,2)
+            #wwb = white bishop on the white square
+            wwb,wbb=Bishop('w',6,1),Bishop('w',3,1)
+            #wk1 = white knight 1
+            wk1,wk2=Knight('w',7,1),Knight('w',2,1)
+            #wr1 = white rook 1
+            wr1,wr2=Rook('w',1,1),Rook('w',8,1)
+            wQ=Queen('w',4,1)
+            wK=King('w',5,1)
+
+            #create the black pieces
+            bAp,bBp,bCp,bDp,bEp,bFp,bGp,bHp=Pawn('b',1,7),Pawn('b',2,7),Pawn('b',3,7),Pawn('b',4,7),Pawn('b',5,7),Pawn('b',6,7),Pawn('b',7,7),Pawn('b',8,7)
+            bwb,bbb=Bishop('b',6,8),Bishop('b',3,8)
+            bk1,bk2=Knight('b',7,8),Knight('b',2,8)
+            br1,br2=Rook('b',1,8),Rook('b',8,8)
+            bQ=Queen('b',4,8)
+            bK=King('b',5,8)
+
+            self.pieces = {'w':[wAp,wBp,wCp,wDp,wEp,wFp,wGp,wHp,wwb,wbb,wk1,wk2,wr1,wr2,wQ,wK],
+                           'b':[bAp,bBp,bCp,bDp,bEp,bFp,bGp,bHp,bwb,bbb,bk1,bk2,br1,br2,bQ,bK]}
+
+        else:
+            self.pieces = {'w':[],
+                           'b':[]}
+            #position=[[color,type of piece, file, row, first_move (if r or K)]... ]
+            for pos in position:
+                if pos[1]=='p':
+                    self.pieces[pos[0]].append(Pawn(pos[0],pos[2],pos[3]))
+                elif pos[1]=='b':
+                    self.pieces[pos[0]].append(Bishop(pos[0],pos[2],pos[3]))
+                elif pos[1]=='k':
+                    self.pieces[pos[0]].append(Knight(pos[0],pos[2],pos[3]))
+                elif pos[1]=='r':
+                    self.pieces[pos[0]].append(Rook(pos[0],pos[2],pos[3],pos[4]))
+                elif pos[1]=='Q':
+                    self.pieces[pos[0]].append(Queen(pos[0],pos[2],pos[3]))
+                else:
+                    self.pieces[pos[0]].append(King(pos[0],pos[2],pos[3],pos[4]))
+
+    def get_entire_position(self):
+            position=[]
+
+            for piece in self.pieces['w']:
+                if piece.piece_type=='r' or piece.piece_type=='K':
+                    position.append([piece.color,piece.piece_type,piece.file,piece.row,piece.first_move])
+                else:
+                    position.append([piece.color,piece.piece_type,piece.file,piece.row])
+
+            for piece in self.pieces['b']:
+                if piece.piece_type=='r' or piece.piece_type=='K':
+                    position.append([piece.color,piece.piece_type,piece.file,piece.row,piece.first_move])
+                else:
+                    position.append([piece.color,piece.piece_type,piece.file,piece.row])
+
+            return position
+
+    def get_one_color_position(self,color):
+            position=[]
+            for piece in self.pieces[color]:
+                position.append([piece.file,piece.row])
+            return position
+
+    def get_piece_from_position(self,pos):
+        #pos = [piece.file, piece.row]
+
+        for piece in self.pieces['w']:
+            if piece.file==pos[0] and piece.row==pos[1]:
+                return piece
+        for piece in self.pieces['b']:
+            if piece.file==pos[0] and piece.row==pos[1]:
+                return piece
+
+        return 0 #if no piece found in this position it returns 0
+
+    def is_checked(self):
+
+        #getting the white king position
+        for piece in self.pieces['w']:
+            if piece.piece_type=='K':
+                break
+        white_king_pos = [piece.file,piece.row]
+
+        #looking for checks
+        for piece in self.pieces['b']:
+            vision = piece.vision(self)
+            if white_king_pos in vision:
+                return 'w' #returns 'w' if the white king is checked
+
+        #getting the black king position
+        for piece in self.pieces['b']:
+            if piece.piece_type=='K':
+                break
+        black_king_pos = [piece.file,piece.row]
+
+        #looking for checks
+        for piece in self.pieces['w']:
+            vision = piece.vision(self)
+            if black_king_pos in vision:
+                return 'b' #returns 'b' if the black king is checked
+
+        return 0 # if no one is checked
+
+    def possible_moves(self,color):
+        possible_moves = [] #[[piece,move],...]
+
+        for piece in self.pieces[color]:
+            for move in piece.vision(self):
+                position_to_test = self.get_entire_position()
+
+                for i,pos in enumerate(position_to_test):
+                    if pos==[color,piece.piece_type,piece.file,piece.row]:
+                        break
+
+                position_to_test[i]=[color,piece.piece_type,move[0],move[1]]
+                Position_to_test = Board(0,position_to_test)
+                if color == 'w':
+                    if Position_to_test.is_checked()!='w':
+                        possible_moves.append([piece,move])
+                else:
+                    if Position_to_test.is_checked()!='b':
+                        possible_moves.append([piece,move])
+
+                Position_to_test.delete_all_pieces()
+                del Position_to_test
+
+        return possible_moves
+
+    def delete_all_pieces(self):
+        del self.pieces['w']
+        del self.pieces['b']
+
+    def move(self,piece,move):
+        if move not in piece.vision(self):
+            return 0 #not a legal move
+        else:
+            position_to_test = self.get_entire_position()
+
+            for i,pos in enumerate(position_to_test):
+                if pos==[piece.color,piece.piece_type,piece.file,piece.row]:
+                    break
+
+            position_to_test[i]=[piece.color,piece.piece_type,move[0],move[1]]
+            Position_to_test = Board(0,position_to_test)
+            if piece.color == 'w':
+                if Position_to_test.is_checked()=='w':
+                    return 0
+            else:
+                if Position_to_test.is_checked()=='b':
+                    return 0
+
+            Position_to_test.delete_all_pieces()
+            del Position_to_test
+
+            # we have handled all cases of illegal moves
+            # Now we have to handle the case of capture and castling
+
+            if piece.color =='w':
+                if move in self.get_one_color_position('b'):
+                    taken_piece = self.get_piece_from_position(move)
+                    self.pieces['b'].remove(taken_piece)
+                    taken_piece.state = 0
+                    piece.move(move)
+
+                    print('take')
+                else:
+                    if piece.piece_type=='K' and (move[0]-piece.file<-1 or move[0]-piece.file>1):
+                            if move[0]==7: #right castling
+                                rook = self.get_piece_from_position([8,1])
+                                rook.move([6,1])
+                                piece.move(move)
+                            else: #left castling
+                                rook = self.get_piece_from_position([1,1])
+                                rook.move([4,1])
+                                piece.move(move)
+                    else:
+                        piece.move(move)
+            else:
+                if move in self.get_one_color_position('w'):
+                    taken_piece = self.get_piece_from_position(move)
+                    self.pieces['w'].remove(taken_piece)
+                    taken_piece.state = 0
+                    piece.move(move)
+                    print('take')
+                else:
+                    if piece.piece_type=='K' and (move[0]-piece.file<-1 or move[0]-piece.file>1):
+                            if move[0]==7: #right castling
+                                rook = self.get_piece_from_position([8,8])
+                                rook.move([6,8])
+                                piece.move(move)
+                            else: #left castling
+                                rook = self.get_piece_from_position([1,8])
+                                rook.move([4,8])
+                                piece.move(move)
+                    else:
+                        piece.move(move)
+
+            # Then the case of promotion for now only in queen
+            if piece.color =='w':
+                if piece.piece_type=='p' and piece.row==8:
+                    self.pieces['w'].remove(piece)
+                    self.pieces['w'].append(Queen('w',piece.file,piece.row))
+                    piece.state = 0
+
+            else:
+                if piece.piece_type=='p' and piece.row==8:
+                    self.pieces['b'].remove(piece)
+                    self.pieces['b'].append(Queen('b',piece.file,piece.row))
+                    piece.state = 0
---
+++ src/game/pieces/king.py
@@ -0,0 +1,95 @@
+import itertools
+from ..board import board
+
+#creation of the king class
+class King:
+    def __init__(self,color,file,row,first_move=1):
+        self.color = color #lets code 'w' for whites and 'b' for blacks
+        self.piece_type = 'K'
+        self.file = file
+        self.row = row
+        self.first_move = first_move
+
+
+    def move(self,move):
+        self.file=move[0]
+        self.row=move[1]
+        if self.first_move == 1:
+            self.first_move = 0
+
+    def vision(self,board):
+        vision=[]
+        # a king can go anywhere 1 case distance if there is no piece of his color on it or if no opposite color piece vision on it
+        for i,j in itertools.product([-1,0,1], [-1,0,1]):
+            if i==0 and j==0:
+                pass
+            else:
+                if self.file+i<1 or self.file+i>8 or self.row+j<1 or self.row+j>8:
+                    pass
+                else:
+                    if [self.file+i,self.row+j] in board.get_one_color_position(self.color):
+                        pass
+                    else:
+                        vision.append([self.file+i,self.row+j])
+                        # the case of a square occupied by an opposite color piece vision is handled by projecting one move
+                        #into the future to make sure the move is legal (king not checked), same as for pinned pieces
+
+        # now we code castling
+        if self.color=='w':
+            if self.first_move==1:
+
+                #left castling
+                piece = board.get_piece_from_position([1,1])
+                if piece!=0:
+                    if piece.piece_type=='r':
+                        if piece.first_move==1:
+                            if board.is_checked()!='w':
+                                black_pieces_vision=[]
+                                all_pieces_position = board.get_one_color_position('b')+board.get_one_color_position('w')
+                                for piece in board.pieces['b']:
+                                    black_pieces_vision+=piece.vision(board)
+                                if [4,1] not in black_pieces_vision and [2,1] not in all_pieces_position and [3,1] not in all_pieces_position and [4,1] not in all_pieces_position:
+                                    vision.append([3,1])
+
+                #right castling
+                piece = board.get_piece_from_position([8,1])
+                if piece!=0:
+                    if piece.piece_type=='r':
+                        if piece.first_move==1:
+                            if board.is_checked()!='w':
+                                black_pieces_vision=[]
+                                all_pieces_position = board.get_one_color_position('b')+board.get_one_color_position('w')
+                                for piece in board.pieces['b']:
+                                    black_pieces_vision+=piece.vision(board)
+                                if [6,1] not in black_pieces_vision and [6,1] not in all_pieces_position and [7,1] not in all_pieces_position:
+                                    vision.append([7,1])
+
+        else:
+            if self.first_move==1:
+
+                #left castling
+                piece = board.get_piece_from_position([1,8])
+                if piece!=0:
+                    if piece.piece_type=='r':
+                        if piece.first_move==1:
+                            if board.is_checked()!='b':
+                                white_pieces_vision=[]
+                                all_pieces_position = board.get_one_color_position('b')+board.get_one_color_position('w')
+                                for piece in board.pieces['w']:
+                                    white_pieces_vision+=piece.vision(board)
+                                if [4,8] not in white_pieces_vision and [2,8] not in all_pieces_position and [3,8] not in all_pieces_position and [4,8] not in all_pieces_position:
+                                    vision.append([3,8])
+
+                #right castling
+                piece = board.get_piece_from_position([8,8])
+                if piece!=0:
+                    if piece.piece_type=='r':
+                        if piece.first_move==1:
+                            if board.is_checked()!='b':
+                                white_pieces_vision=[]
+                                all_pieces_position = board.get_one_color_position('b')+board.get_one_color_position('w')
+                                for piece in board.pieces['w']:
+                                    white_pieces_vision+=piece.vision(board)
+                                if [6,8] not in black_pieces_vision and [6,8] not in all_pieces_position and [7,8] not in all_pieces_position:
+                                    vision.append([7,8])
+        return vision
