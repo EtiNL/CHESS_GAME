840d7fcdced44c7d2f97d091bbd61e85e34da5bd
Etienne Lanzeray <etienne.lanzeray@gmail.com>
2024-03-05 16:46:15 +0100

    removed useless color tracking on main.py as Board in board.py already does it

--- main.py
+++ main.py
@@ -8,10 +8,8 @@ game = board.Board(1,0)
 "game.move(A_pawn,[1,4])"
 gui_game = graphic_interface.App(game.pieces)
 
-color_to_play = 'w'
-
 while game.result==0:
-    move = gui_game.on_execute(color_to_play)
+    move = gui_game.on_execute(game.color_to_play)
     start_pos, fin_pos = move[0],move[1]
     piece = game.get_piece_from_position([start_pos[0],start_pos[1]])
 
@@ -19,26 +17,20 @@ while game.result==0:
         print('No piece here')
         pass
 
-    elif piece.color != color_to_play:
-        print(f"{'white' if color_to_play=='w' else 'black'} to play")
+    elif piece.color != game.color_to_play:
+        print(f"{'white' if game.color_to_play=='w' else 'black'} to play")
         pass
     else:
         if game.Move(piece, [fin_pos[0],fin_pos[1]])==0:
             gui_game.pieces = game.pieces
-            gui_game.get_pieces(color_to_play)
+            gui_game.get_pieces(game.color_to_play)
             print('not a valid move')
             pass
 
         else:
 
             gui_game.pieces = game.pieces
-            gui_game.get_pieces(color_to_play)
-
-            if color_to_play =='w':
-                color_to_play = 'b'
-
-            else:
-                color_to_play = 'w'
+            gui_game.get_pieces(game.color_to_play)
 
 
 if game.result == 'w':
